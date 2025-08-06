5cfb30952ed6a7112aea98251a4aa8447f52caa5
Kliona Kennet <klionakennet2002@gmail.com>
2025-08-07 01:46:17 +1000

    metrics_store.py created

--- afl-vision-insight/backend/routes/metrics_store.py
+++ afl-vision-insight/backend/routes/metrics_store.py
@@ -1,1 +1,16 @@
+player_tracking_calls = 0
+crowd_monitoring_calls = 0
 
+last_player_tracking_output = {
+    "status": None,
+    "players_detected": None,
+    "video_duration": None,
+    "tracking_accuracy": None
+}
+
+last_crowd_monitoring_output = {
+    "status": None,
+    "estimated_crowd_density": None,
+    "heatmap_generated": None,
+    "detected_people": None
+}
